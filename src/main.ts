2f82c2c0e02a592b26fe7bceebc41bf27bbe91d0
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-23 19:05:39 +0200

    handle errors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { RecipeRepositoryMemory } from "./RecipeRepositoryMemory.js"
 import { SearchRecipes } from "./SearchRecipes.js"
 import { SearchRecipe } from "./CreateRecipes.js"
 import { DeleteRecipe } from "./DeleteRecipe.js"
+import { ZodError } from "zod"
 
 const recipeRepository = new RecipeRepositoryMemory()
 const searchRecipes = new SearchRecipes(recipeRepository)
@@ -36,6 +37,71 @@ router
     await deleteRecipe.delete(name)
     ctx.status = 204
   })
+
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err: unknown) {
+    if (err instanceof ZodError) {
+      ctx.status = 400
+      ctx.body = {
+        status: "error",
+        code: "invalid_params",
+        payload: err.errors,
+      }
+      return
+    }
+    if (err instanceof Error) {
+      if (err.message.match(/does not exist/)) {
+        ctx.status = 404
+        ctx.body = {
+          status: "error",
+          code: "recipe_not_found",
+          payload: {
+            message: err.message,
+          },
+        }
+        return
+      }
+      if (err.message.match(/Recipe name cannot be empty/)) {
+        ctx.status = 400
+        ctx.body = {
+          status: "error",
+          code: "recipe_name_must_not_be_empty",
+          payload: {
+            message: err.message,
+          },
+        }
+        return
+      }
+      if (err.message.match(/Recipe description cannot be empty/)) {
+        ctx.status = 400
+        ctx.body = {
+          status: "error",
+          code: "recipe_description_must_not_be_empty",
+          payload: {
+            message: err.message,
+          },
+        }
+        return
+      }
+
+      ctx.status = 500
+      ctx.body = {
+        status: "error",
+        code: "unknown_error",
+        payload: err,
+      }
+    }
+
+    ctx.status = 500
+    ctx.body = {
+      status: "error",
+      code: "unknown_error",
+      payload: err,
+    }
+  }
+})
 
 app.use(router.routes()).use(router.allowedMethods())
 
